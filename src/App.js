5e66671081691dfd72f181134b2ce32724d68fa6
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2020-12-24 01:35:06 +0800

    added search page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+
 
 
 function App() {
   return (
     <div className="app">
-
-      <Header />
-      <Home />
-
-      {/* Home */}
-        {/* Header */}
-
-        {/* Banner */}
-          {/* Search */}
-
-        {/* Cards */}
-
-        {/* Footer */}
-
-      {/* SearchPage */}
-        {/* Header */}
-        {/* .... */}
+      <Router>
+        <Header />
+        <Switch>
+          <Router path='/search'>
+            <SearchPage />
+          </Router>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 } 
--- src/Banner.js
+++ src/Banner.js
@@ -13,7 +13,7 @@ function Banner() {
                 <Button onClick={() => setShowSearch(!showSearch)}
                     className='banner__searchButton' 
                     variant='outlined'>
-                    Search Dates
+                    {showSearch ? 'Hide' : 'Search Dates'}
                 </Button>
             </div>
             <div className="banner__info">
